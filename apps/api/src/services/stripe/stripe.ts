b124c0dfa0861982dd2d2fd17fc968872c517ac1
AlexPiquard <alex.piquard@hotmail.com>
2023-10-27 08:25:15 +0000

    fix stripe version et type

--- apps/api/src/services/stripe/stripe.ts
+++ apps/api/src/services/stripe/stripe.ts
@@ -2,7 +2,7 @@ import { env } from '~/env.ts';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-08-16',
+  apiVersion: '2023-10-16',
   typescript: true,
 });
 
