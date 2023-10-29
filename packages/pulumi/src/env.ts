d9ddc0e043b6d8f4cc1e642ba9fa1d5a4a63d684
Vahor <nathan.d0601@gmail.com>
2023-10-29 18:28:08 +0100

    add aws secrets

--- packages/pulumi/src/env.ts
+++ packages/pulumi/src/env.ts
@@ -4,7 +4,13 @@ import { z } from 'zod';
 export const env = createEnv({
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-    AWS_ENVIRONMENT: z.enum(['aws-dev']).default('aws-dev'),
+
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
+
+    DOCKER_IMAGE: z.string(),
+    DOCKER_USERNAME: z.string(),
+    DOCKER_PASSWORD: z.string(),
 
     PULUMI_ACCESS_TOKEN: z.string(),
   },
