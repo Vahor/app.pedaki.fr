9cfec103223e2097fd7e9deae233f9b1a757601c
Vahor <nathan.d0601@gmail.com>
2023-11-01 19:01:43 +0100

    format

--- packages/pulumi/src/factory.ts
+++ packages/pulumi/src/factory.ts
@@ -1,18 +1,18 @@
-import type {Provider} from '@pedaki/schema/region.model.ts';
-import {AwsServerProvider} from './stack/aws/provider.ts';
-import {TestServerProvider} from './stack/mock/provider.ts';
-import type {ServerProvider} from './type.ts';
+import type { Provider } from '@pedaki/schema/region.model.ts';
+import { AwsServerProvider } from './stack/aws/provider.ts';
+import { TestServerProvider } from './stack/mock/provider.ts';
+import type { ServerProvider } from './type.ts';
 
 const providers = {
-    AWS: new AwsServerProvider(),
-    test: new TestServerProvider(),
+  AWS: new AwsServerProvider(),
+  test: new TestServerProvider(),
 } as const;
 
 class ServerProviderFactory {
-    public getProvider<P extends Provider>(provider: P): ServerProvider<P> {
-        return providers[provider] as ServerProvider<P>;
-    }
+  public getProvider<P extends Provider>(provider: P): ServerProvider<P> {
+    return providers[provider] as ServerProvider<P>;
+  }
 }
 
 const serverFactory = new ServerProviderFactory();
-export {serverFactory};
+export { serverFactory };
