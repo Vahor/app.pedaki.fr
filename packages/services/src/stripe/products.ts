a447f954a2a666c365f17438c2c2b59f94910bb3
AvaN0x <clement.ricatte57@gmail.com>
2023-11-07 19:34:58 +0100

    chore: :fire: remove unused code

--- packages/services/src/stripe/products.ts
+++ packages/services/src/stripe/products.ts
@@ -2,14 +2,12 @@ import { ProductType } from '@prisma/client';
 import { env } from '~/env.ts';
 
 export interface Product {
-  type: 'plan';
   payment_type: 'subscription';
   priceId: { monthly: string; yearly: string };
 }
 
 export const products = {
   [ProductType.HOSTING]: {
-    type: 'plan',
     payment_type: 'subscription',
     priceId: {
       monthly: env.STRIPE_PRODUCT_HOSTED_MONTHLY_PRICE_ID,
