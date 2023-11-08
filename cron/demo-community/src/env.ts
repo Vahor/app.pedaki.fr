0628a69bed056875a19252889df83eca4e824847
Vahor <nathan.d0601@gmail.com>
2023-11-08 16:19:03 +0100

    env

--- cron/demo-community/src/env.ts
+++ cron/demo-community/src/env.ts
@@ -6,10 +6,10 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     PRISMA_ENCRYPTION_KEY: z.string().min(1).default('secret'),
 
-    APP_DOCKER_HOST: z.string(),
-    APP_DOCKER_ORGANISATION: z.string(),
-    APP_DOCKER_PACKAGE_NAME: z.string(),
-    APP_DOCKER_PASSWORD: z.string(),
+    APP_DOCKER_HOST: z.string().min(1),
+    APP_DOCKER_ORGANISATION: z.string().min(1),
+    APP_DOCKER_PACKAGE_NAME: z.string().min(1),
+    APP_DOCKER_PASSWORD: z.string().min(1),
   },
   runtimeEnv: process.env,
 });
