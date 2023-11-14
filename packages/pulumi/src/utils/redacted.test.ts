49f74b3a25b49f0a39774b4125b5c6816531b0e9
Vahor <nathan.d0601@gmail.com>
2023-11-14 19:47:51 +0100

    add typecheck

--- packages/pulumi/src/utils/redacted.test.ts
+++ packages/pulumi/src/utils/redacted.test.ts
@@ -1,14 +1,18 @@
 import { describe, expect, test } from 'vitest';
 import { redacted, REPLACEMENT } from './redacted.ts';
+import {expectTypeOf} from 'expect-type'
 
 describe('redacted', () => {
   test('ignore non sensitive data', () => {
     const result = redacted({ SOME_KEY: 'value' });
     expect(result).toEqual({ SOME_KEY: 'value' });
+    expectTypeOf(result).toEqualTypeOf<{ SOME_KEY: string }>();
   });
 
   test('redact sensitive data', () => {
     const result = redacted({ AUTH_TOKEN: 'value' });
     expect(result).toEqual({ AUTH_TOKEN: REPLACEMENT });
+    expectTypeOf(result).toEqualTypeOf<{ AUTH_TOKEN: string }>();
   });
+
 });
