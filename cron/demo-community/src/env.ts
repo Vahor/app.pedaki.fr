fe3b8b000b64f94f5afaf111ecacd218819637aa
Vahor <nathan.d0601@gmail.com>
2023-11-15 16:45:22 +0100

    keep old stack demo

---
+++ cron/demo-community/src/env.ts
@@ -0,0 +1,9 @@
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
+
+export const env = createEnv({
+  server: {
+    DELETE_OLD_STACK: z.coerce.boolean().default(false),
+  },
+  runtimeEnv: process.env,
+});
