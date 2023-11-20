3a609e88d5f1834e88258eaf71434e69bd923607
Vahor <nathan.d0601@gmail.com>
2023-11-20 13:23:18 +0100

    remove stack router

---
+++ apps/api/src/router/routers/version/index.ts
@@ -0,0 +1,26 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import { publicProcedure, router } from '../../trpc.ts';
+
+export const versionRouter = router({
+  getMany: publicProcedure
+    .input(
+      z.object({
+        product: z.enum(['pedaki-community', 'pedaki-premium']),
+      }),
+    )
+    .output(
+      z.array(
+        z.object({
+          version: z.string(),
+        }),
+      ),
+    )
+    .meta({ method: 'GET', path: '/versions' })
+    .query(() => {
+      throw new TRPCError({
+        code: 'NOT_IMPLEMENTED',
+        message: 'Not implemented',
+      });
+    }),
+});
