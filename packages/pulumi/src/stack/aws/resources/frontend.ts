7449c8c126a67fe72e8a82b1d8d88a667729e8a7
Vahor <me@vahor.fr>
2023-11-26 23:28:02 +0100

    moooooooooooooooore

--- packages/pulumi/src/stack/aws/resources/frontend.ts
+++ packages/pulumi/src/stack/aws/resources/frontend.ts
@@ -138,7 +138,7 @@ services:
 <filter>
   @type record_transformer
   <record>
-    $baselime.service \\"WORKSPACE_ID-fluentd\\"
+    \\"$baselime.service\\" \\"WORKSPACE_ID-fluentd\\"
   </record>
 </filter>
 
