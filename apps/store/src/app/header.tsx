da4f37ca61902824255c2841fcaddce173fd3b9e
Vahor <me@vahor.fr>
2023-11-30 20:33:46 +0100

    StepIndicatorHorizontal

--- apps/store/src/app/header.tsx
+++ apps/store/src/app/header.tsx
@@ -5,15 +5,14 @@ import { env } from '~/env.mjs';
 import Link from 'next/link';
 import React from 'react';
 
+
 const Header = () => {
   return (
     <div className="relative border-b bg-primary px-6 py-4 md:px-12">
       <header className="mx-auto max-w-screen-2xl">
         <div className="flex items-center justify-between gap-4">
-          <div className="flex items-center md:gap-2">
-            <Logo />
-            <CurrentPage />
-          </div>
+          <Logo />
+          <CurrentPage />
           <div>
             <Button variant="outline" asChild>
               <Link href={env.NEXT_PUBLIC_DOCS_URL}>Documentation</Link>
--- apps/store/src/components/header/current-page.tsx
+++ apps/store/src/components/header/current-page.tsx
@@ -1,66 +1,45 @@
 'use client';
 
-import { Button } from '@pedaki/design/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@pedaki/design/ui/dropdown-menu';
-import { IconChevronDown, IconChevronRight } from '@pedaki/design/ui/icons';
+import { StepIndicatorHorizontal } from '~/components/step-indicator/horizontal';
+import { StepIndicatorHorizontalItem } from '~/components/step-indicator/horizontal/item.tsx';
 import { usePathname } from 'next/navigation';
 import React from 'react';
+
 
 const page_mapping = [
   {
     path: '/new',
-    translationKey: 'step_1',
+    translationKey: 'Informations',
   },
   {
     path: '/new/pending',
-    translationKey: 'step_2',
+    translationKey: 'Abonnement',
   },
   {
     path: '/new/invitations',
-    translationKey: 'step_3',
+    translationKey: 'Invitations',
   },
   {
     path: '/new/ready',
-    translationKey: 'step_4',
+    translationKey: 'Ready',
   },
 ] as const;
-
-// Ex: 404 page
-const unknown_page = {
-  translationKey: 'unknown',
-};
 
 const CurrentPage = () => {
   const pathname = usePathname();
   const activeIndex = page_mapping.findIndex(page => page.path === pathname);
-  const translationKey =
-    activeIndex === -1 ? unknown_page.translationKey : page_mapping[activeIndex]!.translationKey;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="transparent" size="sm" className="flex items-center md:gap-2">
-          <span>{translationKey}.large</span>
-          <IconChevronDown className="h-4 w-4 text-secondary" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="start" side="top">
-        {page_mapping.map((page, index) => {
-          const isCurrent = index === activeIndex;
-          return (
-            <DropdownMenuLabel key={page.path} className="flex items-center gap-2 font-normal">
-              {isCurrent ? <IconChevronRight className="h-4 w-4" /> : <span className="h-4 w-4" />}
-              <span>{page.translationKey}.small</span>
-            </DropdownMenuLabel>
-          );
-        })}
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <StepIndicatorHorizontal>
+      {page_mapping.map((page, index) => (
+        <StepIndicatorHorizontalItem
+          number={index + 1}
+          text={page.translationKey}
+          key={index}
+          status={index === activeIndex ? 'active' : index < activeIndex ? 'completed' : 'pending'}
+        />
+      ))}
+    </StepIndicatorHorizontal>
   );
 };
 
