c311ff19bc3296f7ff34f50016ced51956c7b508
Vahor <me@vahor.fr>
2023-12-08 10:47:46 +0100

    imports

--- apps/store/src/app/footer.tsx
+++ apps/store/src/app/footer.tsx
@@ -1,10 +1,4 @@
-import { Button } from '@pedaki/design/ui/button';
-import { IconBookText } from '@pedaki/design/ui/icons';
 import LanguageSelector from '~/components/footer/LanguageSelector';
-import CurrentPage from '~/components/header/current-page.tsx';
-import Logo from '~/components/header/logo.tsx';
-import { env } from '~/env.mjs';
-import Link from 'next/link';
 import React from 'react';
 
 const Footer = () => {
