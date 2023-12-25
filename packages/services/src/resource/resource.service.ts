610edbb13115aa27a8df48248a190bcf2f732524
Vahor <me@vahor.fr>
2023-12-25 11:20:34 +0100

    fix: 5 minutes expected

--- packages/services/src/resource/resource.service.ts
+++ packages/services/src/resource/resource.service.ts
@@ -125,7 +125,7 @@ class ResourceService {
           },
         );
 
-        // we expect the server to be created in the next 15 minutes
+        // we expect the server to be created in the next 5 minutes
         setTimeout(
           () => {
             void workspaceService.updateExpectedStatus({
@@ -134,7 +134,7 @@ class ResourceService {
               whereStatus: 'CREATING',
             });
           },
-          15 * 60 * 1000,
+          5 * 60 * 1000,
         );
 
         logger.info({
