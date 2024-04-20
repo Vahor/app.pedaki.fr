0a5bcb072cb3824866ec5d9cbf5192944bd144dc
Vahor <me@vahor.fr>
2024-04-20 17:37:46 +0200

    fix: set telemetry attributes before passing to another to bypass errors

--- apps/api/src/router/middleware/telemetry.middleware.ts
+++ apps/api/src/router/middleware/telemetry.middleware.ts
@@ -6,15 +6,19 @@ export const withTelemetry = t.middleware(
 	async ({ rawInput, path, type, ctx, next }) => {
 		const tracer = trace.getTracer("@baselime/trpc"); // use baselime to keep the logo
 		return tracer.startActiveSpan(`${path} - ${type}`, async (span) => {
-			const result = await next();
 			span.setAttributes(
 				flatten({
 					input: rawInput,
 					path: path,
 					type: type,
-					ok: result.ok,
+					fromAddr:
+						ctx.req.headers["x-forwarded-for"] ||
+						ctx.req.connection.remoteAddress,
 				}),
 			);
+			const result = await next();
+			span.setAttribute("ok", result.ok);
+
 			span.end();
 			return result;
 		});
